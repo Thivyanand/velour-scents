5cfe24e67e7266a909a4c5daa947ac8e46a4016c
Thivyanand S T <nc.sc.u4cse25041@nc.students.amrita.edu>
2026-06-03 16:23:27 +0530

    Add AdSense auto ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 import Footer from "../components/footer";
 import "./globals.css";
 
@@ -16,7 +17,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Velour Scents",
-  description: "Luxury Alcohol-Free Attars",
+  description: "Premium Luxury Alcohol-Free Attars",
 };
 
 export default function RootLayout({
@@ -30,6 +31,14 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8713834211656149"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+
         <main className="flex-1">
           {children}
         </main>
